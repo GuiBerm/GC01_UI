50693217cb1f95d7052ad8e2d65408ed069a25e7
guille <gubermejo@alumnos.unex.es>
2024-11-22 18:15:23 +0100

    SCRUM-182 ST40. Añadir controles para seleccionar un perfil activo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import ProfileSelection from './components/User/ProfileSelection';
 
+import ProtectedRoute from './components/ProtectedRoute'; // Rutas protegidas
 import './styles/styles.css';
 import { AuthProvider } from './context/AuthContext';
 
@@ -12,6 +15,25 @@ function App() {
                 <Routes>
                     {/* Ruta pública para Login */}
                     <Route path="/login" element={<Login />} />
+
+                    {/* Rutas protegidas para usuarios autenticados */}
+                    <Route
+                        path="/user/profile"
+                        element={
+                            <ProtectedRoute>
+                                <ProfileSelection />
+                            </ProtectedRoute>
+                        }
+                    />
+                    />
+                    <Route
+                        path="/logout"
+                        element={
+                            <ProtectedRoute>
+                                <Logout />
+                            </ProtectedRoute>
+                        }
+                    />
 
                 </Routes>
             </Router>
